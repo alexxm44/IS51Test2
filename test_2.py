4715e305560bc50d6f342afdf75bae4a467b3bb1
Alex M <Alex@Alexs-Air.attlocal.net>
2021-04-30 03:40:14 -0700

    finish structured english

---
+++ test_2.py
@@ -0,0 +1,9 @@
+"""
+create a list to store 24 numbers (float)
+capture user's input 24 times for their grades
+each time we capture the user's, we append the number to the list
+sort the list ascending, then splice the items before index 13
+output we have 13 highest number on the list, we sum them up and divide them by13
+output a message to the user
+end
+"""
